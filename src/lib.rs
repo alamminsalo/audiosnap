07900b2ebb0d7cbcab85a3234c9c5a2c6929f9b2
alamminsalo <antti.lamminsalo@protonmail.com>
2018-06-17 11:17:02 +0300

    c exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 extern crate hound;
 
+#[macro_use]
+extern crate lazy_static;
+
+use std::sync::Mutex;
+
+// library version
+const LIB_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+// contains loaded sample data
+lazy_static! {
+    static ref DATA: Mutex<Vec<i16>> = Mutex::new(vec![]);
+    static ref SPLITS: Mutex<Vec<u32>> = Mutex::new(vec![]);
+}
+
 fn default_spec() -> hound::WavSpec {
     hound::WavSpec{
         channels: 1,
@@ -23,23 +37,30 @@ pub fn print_spec(inputfile: &str) {
 }
 
 // Loads wav sample as vec<i16>
-pub fn load_file(inputfile: &str) -> Vec<i16> {
+pub fn load_file(inputfile: &str) {
     let mut reader = hound::WavReader::open(inputfile).unwrap();
-    reader.samples::<i16>()
+
+    // Set global DATA
+    let mut d = DATA.lock().unwrap();
+    d.clear();
+    d.append(&mut reader.samples::<i16>()
         .map(|s| s.unwrap() as i16)
-        .collect()
+        .collect());
 }
 
 // Split data and return vector of indexes (split points)
-pub fn split(data: &Vec<i16>, treshold: f32) -> Vec<u32> {
-    let ceil = (std::i16::MAX as f32 * treshold) as i16;
-    data.iter()
+pub fn split(treshold: i16) -> usize {
+    let data = DATA.lock().unwrap();
+    let mut s = SPLITS.lock().unwrap();
+    s.clear();
+    s.append(&mut data.iter()
         .enumerate()
         .filter(|&(i, s)| {
-            i == 0 || s.abs() > ceil
+            i == 0 || *s > treshold
         })
-    .map(|(i,_)| i as u32)
-        .collect()
+        .map(|(i,_)| i as u32)
+        .collect());
+    s.len()
 }
 
 pub fn smooth(splits: &Vec<(usize, i16)>, tolerance: f32) -> Vec<(usize, i16)> {
@@ -77,3 +98,46 @@ pub fn write_frame(frame: &(u32,u32), data: &Vec<i16>, outputfile: &str) {
     writer.finalize().unwrap();
 }
 
+
+// c api
+extern crate libc;
+use libc::{c_char,int16_t,size_t};
+use std::ffi::{CStr,CString};
+
+
+// Check file size before allocating buffer
+#[no_mangle]
+pub extern fn c_file_len() -> size_t {
+    DATA.lock().unwrap().len() as usize
+}
+
+#[no_mangle]
+pub extern fn c_load_file(file: *const c_char) -> size_t {
+    let fstr = unsafe{ CStr::from_ptr(file) }.to_str().unwrap();
+    load_file(fstr);
+    c_file_len()
+}
+
+#[no_mangle]
+pub extern fn c_data(buf: *mut int16_t, _len: size_t) -> size_t {
+    let data = DATA.lock().unwrap();
+    // copy contents of values to buf
+    for (i,v) in data.iter().take(_len).enumerate() {
+        unsafe {
+            let ptr = buf.offset(i as isize) as *mut i16;
+            *ptr = v.clone();
+        }
+    }
+
+    std::cmp::max(data.len(),_len)
+}
+
+#[no_mangle]
+pub extern fn c_split(ceil: int16_t) -> size_t {
+    split(ceil as i16)
+}
+
+#[no_mangle]
+pub extern fn c_version() -> *const c_char {
+    CString::new(LIB_VERSION).unwrap().into_raw()
+}
